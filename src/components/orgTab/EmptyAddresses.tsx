bb1f87608d396bc53a065c8ed1209a871e1d64eb
hoco <harrison.comfort@gmail.com>
2024-12-12 14:00:06 -0500

    redirect to machine page if no keys

--- src/components/orgTab/EmptyAddresses.tsx
+++ src/components/orgTab/EmptyAddresses.tsx
@@ -12,11 +12,11 @@ export const EmptyAddresses: React.FC<EmptyAddressesProps> = () => {
     <div className="flex flex-col w-full justify-center items-center gap-5">
       <img className="w-[76px]" src={server} alt="server" />
       <div className="w-[552px]">
-        <span className="justify-center text-base font-medium text-ivywhite">Add Operator Address </span>
-        <span className="justify-center text-sidebarColor">in order to view your metrics on a per address basis</span>
+        <span className="justify-center text-base font-medium text-ivywhite">Add operator address </span>
+        <span className="justify-center text-sidebarColor">on the Nodes Overview tab in order to view your metrics on a per address basis</span>
       </div>
-      <Link to="edit/keys">
-        <button className="py-2.5 px-4 bg-accent/[0.10] border border-accent text-accent rounded-lg text-base font-semibold">+ Add Address</button>
+      <Link to="/machines">
+        <button className="py-2.5 px-4 bg-accent/[0.10] border border-accent text-accent rounded-lg text-base font-semibold">Nodes Overview</button>
       </Link>
       <div className="w-[552px]">
         <span className="justify-center text-sidebarColor">Just deployed IvyNet? It can take a few minutes to display.</span>

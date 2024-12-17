0d05173b530224e8dc9d180e6523cd7e6f2d88ea
hoco <harrison.comfort@gmail.com>
2024-12-17 16:40:10 +0100

    internal front end candidate changes

---
+++ src/components/machinesTab/ChainCell.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getChainLabel } from '../../utils/UiMessages';
+
+interface ChainCellProps {
+  chain: string | null;
+  avsName: string;
+  machineId: string;
+}
+
+const ChainCell: React.FC<ChainCellProps> = ({ chain, avsName, machineId }) => {
+  const chainLabel = getChainLabel(chain);
+
+  if (!chainLabel) {
+    return (
+      <div className="w-24 flex justify-center">
+        <Link to={`/machines/edit/${avsName}/${machineId}`}>
+          <div className="px-4 py-2 rounded-lg bg-bgButton hover:bg-textGrey text-textSecondary text-md">
+            Add
+          </div>
+        </Link>
+      </div>
+    );
+  }
+
+  return <span className="text-textSecondary">{chainLabel}</span>;
+};
+
+export default ChainCell;
---
+++ src/components/machinesTab/Rescan.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { CodeModalUpdate } from "../shared/CodeModalUpdate";
+
+interface RescanModalProps {
+  onClose?: () => void;
+  isOpen?: boolean;
+  machineId?: string;
+}
+
+export const RescanModal: React.FC<RescanModalProps> = ({
+  onClose,
+  isOpen = true,
+  machineId
+}) => {
+  const title = "Not seeing your latest AVSs? Rescan!";
+  const code = `https://docs.ivynet.dev/docs/client/QuickstartGuide`;
+
+  const handleClose = () => {
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  return (
+    <CodeModalUpdate
+      title={title}
+      code={code}
+      isOpen={isOpen}
+      onClose={handleClose}
+    />
+  );
+};
